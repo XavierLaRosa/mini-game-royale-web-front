4d2c516dcef390b8aac089b5e4277cebcdcbc85f
Xavier La Rosa <38988734+XavierLaRosa@users.noreply.github.com>
2020-11-20 02:19:48 -0500

    added user login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AppService } from 'src/app/app.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  username: string = ''
+  password: string = ''
+  constructor(private appService: AppService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  login() {
+    var body = {username: this.username, password: this.password}
+    console.log("body to send: ", body)
+    this.appService.loginUser(body).subscribe({
+      next: data => {
+        console.log("API Success: ", data)
+        this.router.navigateByUrl('/home')
+      },
+      error: error => {
+        console.log("API Error: ", error)
+      }
+    })
+  }
 }
