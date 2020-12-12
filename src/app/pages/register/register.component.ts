66f178564b18f481fd87925c02325eaa58d39c29
Xavier La Rosa <xavier.a.larosa@gmail.com>
2020-12-12 16:12:49 -0500

    user validation for register page

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -10,21 +10,30 @@ import { AppService } from 'src/app/app.service';
 export class RegisterComponent implements OnInit {
   username: string = ''
   password: string = ''
+  password2: string = ''
   constructor(private appService: AppService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
   register() {
-    var body = {username: this.username, password: this.password}
-    this.appService.regsiterUser(body).subscribe({ // register user with credentials
-      next: data => {
-        console.log("Register API Success: ", data)
-        this.router.navigateByUrl('/')
-      },
-      error: error => {
-        console.log("Register API Error: ", error)
-      }
-    })
+    if(this.validate(this.username, this.password, this.password2)) {
+      var body = {username: this.username, password: this.password}
+      this.appService.regsiterUser(body).subscribe({ // register user with credentials
+        next: data => {
+          console.log("Register API Success: ", data)
+          this.router.navigateByUrl('/')
+        },
+        error: error => {
+          console.log("Register API Error: ", error)
+        }
+      })
+    } else {
+      console.log("Unable to create account! Either username already exists or passwords do not match.")
+    }
+  }
+
+  validate(u: string, p: string, p2: string): boolean {
+    return u && (p == p2)
   }
 }
