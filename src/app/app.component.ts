73e07d187ebab9ac6cff97958dd573eb3110e3af
Xavier La Rosa <xavier.a.larosa@gmail.com>
2020-12-27 03:29:08 -0500

    music works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { faUserFriends } from '@fortawesome/free-solid-svg-icons';
+import {Howl, Howler} from 'howler';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,20 @@ import { faUserFriends } from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  faUserFriends = faUserFriends;
-  title = 'mini-game-royale-web-front';
+  faUserFriends = faUserFriends
+  title = 'mini-game-royale-web-front'
+  sound = new Howl({
+    src: ['../assets/mp3/What-if-a-Day-harp/What-if-a-Day.mp3']
+  });
+
+ constructor() {
+ }
+
+ ngOnInit() {
+   // Play the sound.
+   //this.sound.play();
+
+   // Change global volume.
+   Howler.volume(0.3);
+ }
 }
