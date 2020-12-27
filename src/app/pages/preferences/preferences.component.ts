edc6083e51f7e0d4e7dec88eeef34fcf5eba34f2
Xavier La Rosa <38988734+XavierLaRosa@users.noreply.github.com>
2020-12-27 17:23:21 -0500

    added static input fields for user details preferences page

--- src/app/pages/preferences/preferences.component.ts
+++ src/app/pages/preferences/preferences.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { faChevronLeft, faCog } from '@fortawesome/free-solid-svg-icons';
+import { AppService, User } from 'src/app/app.service';
 
 @Component({
   selector: 'app-preferences',
@@ -9,10 +10,22 @@ import { faChevronLeft, faCog } from '@fortawesome/free-solid-svg-icons';
 export class PreferencesComponent implements OnInit {
   faCog = faCog
   faChevronLeft = faChevronLeft
+  username: string
+  password: string
+  password2: string
   isMuted: boolean = false
-  constructor() { }
+  constructor(public appService: AppService) { }
 
   ngOnInit(): void {
+    this.appService.getUser(AppService.id).subscribe({ // get user data
+      next: data => {
+        console.log("User API Success: ", data)
+        AppService.user = data as User
+      },
+      error: error => {
+        console.log("User API Error: ", error)
+      }
+    })
   }
 
 }
